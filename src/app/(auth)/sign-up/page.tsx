600a96c6043d125319d89c9c0674aff346057d50
firatgoktepe <firatgoktepe1986@gmail.com>
2024-10-14 20:11:28 +0300

    style: page and layout components modified

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,5 @@
+const SignUpPage = () => {
+  return <div className="">Sign up Page</div>;
+};
+
+export default SignUpPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import { Button } from "@/components/ui/button";
-import { TestComponent } from "@/features/test";
 
 export default function Home() {
   return (
     <div>
       <Button variant="primary">Click me</Button>
       <p className="text-red-500 font-semibold">Firat</p>
-      <TestComponent />
     </div>
   );
 }
