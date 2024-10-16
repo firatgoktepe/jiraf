008b1604d4d6a7409482b1e9bd936f1d515fc165
firatgoktepe <firatgoktepe1986@gmail.com>
2024-10-16 21:09:15 +0300

    feat: auth session & logout added

--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -2,7 +2,8 @@ import { Hono } from "hono"
 import { zValidator } from "@hono/zod-validator"
 import { createAdminClient } from "@/lib/appwrite"
 import { ID } from "node-appwrite"
-import { setCookie } from "hono/cookie";
+import { deleteCookie, setCookie } from "hono/cookie";
+import { sessionMiddleware } from "@/lib/session-middleware";
 import { AUTH_COOKIE } from "../constants";
 
 import { loginSchema, registerSchema } from "../schemas"
@@ -14,9 +15,21 @@ const app = new Hono()
         async (c) => {
             const { email, password } = c.req.valid("json")
 
-            console.log({ email, password })
+            const { account } = await createAdminClient();
+            const session = await account.createEmailPasswordSession(
+                email,
+                password,
+            );
 
-            return c.json({ email, password })
+            setCookie(c, AUTH_COOKIE, session.secret, {
+                path: "/",
+                httpOnly: true,
+                secure: true,
+                sameSite: "strict",
+                maxAge: 60 * 60 * 24 * 30,
+            });
+
+            return c.json({ success: true });
         }
     )
     .post(
@@ -26,7 +39,7 @@ const app = new Hono()
             const { name, email, password } = c.req.valid("json")
 
             const { account } = await createAdminClient();
-            const user = await account.create(
+            await account.create(
                 ID.unique(),
                 email,
                 password,
@@ -46,8 +59,16 @@ const app = new Hono()
                 maxAge: 60 * 60 * 24 * 30,
             });
 
-            return c.json({ data: user });
+            return c.json({ success: true });
         }
-    );
+    )
+
+    .post("/logout", sessionMiddleware, (c) => {
+        const account = c.get("account");
+
+        deleteCookie(c, AUTH_COOKIE);
+
+        return c.json({ success: true });
+    })
 
 export default app
