dc7d23d44f1dd1814eec48fd303847fd61147ff8
firatgoktepe <firatgoktepe1986@gmail.com>
2024-10-16 17:03:09 +0300

    fix: return values modified

--- src/features/auth/server/route.ts
+++ src/features/auth/server/route.ts
@@ -7,7 +7,13 @@ const app = new Hono()
     .post(
         "/login",
         zValidator("json", loginSchema),
-        (c) => { return c.json({ success: "ok" }) }
+        async (c) => {
+            const { email, password } = c.req.valid("json")
+
+            console.log({ email, password })
+
+            return c.json({ email, password })
+        }
     )
 
 export default app
