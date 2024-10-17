25f0113d8892bb69ae46e40c78329990a9039e2d
firatgoktepe <firatgoktepe1986@gmail.com>
2024-10-17 19:00:45 +0300

    fix: pending status added

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Link from "next/link";
 import { z } from "zod";
 import { FcGoogle } from "react-icons/fc";
@@ -20,7 +22,7 @@ import { loginSchema } from "../schemas";
 import { useLogin } from "../api/use-login";
 
 export const SignInCard = () => {
-  const { mutate } = useLogin();
+  const { mutate, isPending } = useLogin();
   const form = useForm<z.infer<typeof loginSchema>>({
     resolver: zodResolver(loginSchema),
     defaultValues: {
@@ -77,7 +79,7 @@ export const SignInCard = () => {
                 </FormItem>
               )}
             />
-            <Button size="lg" disabled={false} className="w-full">
+            <Button size="lg" disabled={isPending} className="w-full">
               Login
             </Button>
           </form>
@@ -88,7 +90,7 @@ export const SignInCard = () => {
       </div>
       <CardContent className="p-7 flex flex-col gap-y-4">
         <Button
-          disabled={false}
+          disabled={isPending}
           size="lg"
           className="w-full"
           variant="secondary"
@@ -97,7 +99,7 @@ export const SignInCard = () => {
           Login with Google
         </Button>
         <Button
-          disabled={false}
+          disabled={isPending}
           size="lg"
           className="w-full"
           variant="secondary"
