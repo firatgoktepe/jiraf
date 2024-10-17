1081010b6d97c65bef225283441f0e540f60cf8d
firatgoktepe <firatgoktepe1986@gmail.com>
2024-10-17 11:57:42 +0300

    feat: logout api added & its implementations to the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,27 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+import { useCurrent } from "@/features/auth/api/use-current";
+import { useLogout } from "@/features/auth/api/use-logout";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
+  const router = useRouter();
+  const { data, isLoading } = useCurrent();
+  const { mutate } = useLogout();
+
+  useEffect(() => {
+    if (!data && !isLoading) {
+      router.push("/sign-in");
+    }
+  }, [data]);
+
   return (
     <div>
-      <Button variant="primary">Click me</Button>
-      <p className="text-red-500 font-semibold">Firat</p>
+      Only visible to authenticated users.
+      <Button onClick={() => mutate()}>Logout</Button>
     </div>
   );
 }
