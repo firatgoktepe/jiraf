593d07ea01a97210e2c0838a40f67248821184bc
firatgoktepe <firatgoktepe1986@gmail.com>
2024-10-17 18:45:21 +0300

    fix: refresh problem solved

--- src/features/auth/api/use-logout.ts
+++ src/features/auth/api/use-logout.ts
@@ -1,3 +1,5 @@
+import { useRouter } from "next/navigation";
+
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { InferResponseType } from "hono";
 
@@ -6,6 +8,7 @@ import { client } from "@/lib/rpc";
 type ResponseType = InferResponseType<typeof client.api.auth.logout["$post"]>
 
 export const useLogout = () => {
+    const router = useRouter();
     const queryClient = useQueryClient();
 
     const mutation = useMutation<
@@ -17,6 +20,7 @@ export const useLogout = () => {
             return await response.json()
         },
         onSuccess: () => {
+            router.refresh();
             queryClient.invalidateQueries({ queryKey: ["current"] })
         }
 
--- src/features/auth/api/use-register.ts
+++ src/features/auth/api/use-register.ts
@@ -1,4 +1,6 @@
-import { useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { InferRequestType, InferResponseType } from "hono";
 
 import { client } from "@/lib/rpc";
@@ -7,6 +9,9 @@ type ResponseType = InferResponseType<typeof client.api.auth.register["$post"]>
 type RequestType = InferRequestType<typeof client.api.auth.register["$post"]>
 
 export const useRegister = () => {
+    const queryClient = useQueryClient();
+    const router = useRouter();
+
     const mutation = useMutation<
         ResponseType,
         Error,
@@ -16,7 +21,10 @@ export const useRegister = () => {
             const response = await client.api.auth.register["$post"]({ json })
             return await response.json()
         },
-
+        onSuccess: () => {
+            router.refresh();
+            queryClient.invalidateQueries({ queryKey: ["current"] })
+        }
 
     })
     return mutation
