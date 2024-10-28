9f7ec140bdbb7c4ca2198aed9f6d22c66e726003
firatgoktepe <firatgoktepe1986@gmail.com>
2024-10-28 15:21:33 +0300

    feat: add uploaded image url functionality

--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -5,7 +5,7 @@ import { ID } from "node-appwrite";
 import { createWorkspaceSchema } from "../shemas";
 
 import { sessionMiddleware } from "@/lib/session-middleware";
-import { DATABASE_ID, WORKSPACES_ID } from "@/config";
+import { DATABASE_ID, IMAGES_BUCKET_ID, WORKSPACES_ID } from "@/config";
 
 const app = new Hono()
     .post(
@@ -14,9 +14,27 @@ const app = new Hono()
         sessionMiddleware,
         async (c) => {
             const databases = c.get("databases");
+            const storage = c.get("storage");
             const user = c.get("user");
 
-            const { name } = c.req.valid("json");
+            const { name, image } = c.req.valid("json");
+
+            let uploadedImageUrl: string | undefined;
+
+            if (image instanceof File) {
+                const file = await storage.createFile(
+                    IMAGES_BUCKET_ID,
+                    ID.unique(),
+                    image,
+                );
+
+                const arrayBuffer = await storage.getFilePreview(
+                    IMAGES_BUCKET_ID,
+                    file.$id,
+                );
+
+                uploadedImageUrl = `data:image/png;base64,${Buffer.from(arrayBuffer).toString("base64")}`;
+            }
 
             const workspace = await databases.createDocument(
                 DATABASE_ID,
@@ -25,6 +43,7 @@ const app = new Hono()
                 {
                     name,
                     userId: user.$id,
+                    imageUrl: uploadedImageUrl,
                 },
             );
             return c.json({ data: workspace });
