770b0710db2abab264bf3519ab027f776fe489c8
firatgoktepe <firatgoktepe1986@gmail.com>
2024-10-28 20:00:07 +0300

    feat: create get api & fetch workspaces

--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -8,6 +8,15 @@ import { sessionMiddleware } from "@/lib/session-middleware";
 import { DATABASE_ID, IMAGES_BUCKET_ID, WORKSPACES_ID } from "@/config";
 
 const app = new Hono()
+    .get("/", sessionMiddleware, async (c) => {
+        const databases = c.get("databases");
+
+        const workspaces = await databases.listDocuments(
+            DATABASE_ID,
+            WORKSPACES_ID,
+        );
+        return c.json({ data: workspaces });
+    })
     .post(
         "/",
         zValidator("form", createWorkspaceSchema),
