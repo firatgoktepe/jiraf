19c0e7cc88bcec8e58cab2c0d11a11876d2ecb0b
firatgoktepe <firatgoktepe1986@gmail.com>
2024-10-28 14:28:02 +0300

    feat: add success/error messages for login/out & register

--- src/features/auth/api/use-login.ts
+++ src/features/auth/api/use-login.ts
@@ -1,3 +1,4 @@
+import { toast } from "sonner";
 import { useRouter } from "next/navigation";
 
 import { useMutation, useQueryClient } from "@tanstack/react-query";
@@ -18,13 +19,21 @@ export const useLogin = () => {
     >({
         mutationFn: async ({ json }) => {
             const response = await client.api.auth.login["$post"]({ json })
+
+            if (!response.ok) {
+                throw new Error("Failed to log in!");
+            }
+
             return await response.json()
         },
         onSuccess: () => {
+            toast.success("Logged in!");
             router.refresh();
             queryClient.invalidateQueries({ queryKey: ["current"] })
-        }
-
+        },
+        onError: () => {
+            toast.error("Failed to log in!");
+        },
 
     })
     return mutation
