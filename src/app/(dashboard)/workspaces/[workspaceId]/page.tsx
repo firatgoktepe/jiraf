437f8369c2ed349b097172351f217b76b931f557
firatgoktepe <firatgoktepe1986@gmail.com>
2024-10-30 17:13:29 +0300

    feat: create example workspace id page

---
+++ src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
@@ -0,0 +1,10 @@
+const WorkspaceIdPage = () => {
+  return (
+    <div className="">
+      Workspace Id
+      <p>This is the workspace id page</p>
+    </div>
+  );
+};
+
+export default WorkspaceIdPage;
