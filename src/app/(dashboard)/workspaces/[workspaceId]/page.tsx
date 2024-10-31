3036176bd65053c5de820ddd4882265d2dcc4970
firatgoktepe <firatgoktepe1986@gmail.com>
2024-10-31 14:50:56 +0300

    fix: protect workspace id page

--- src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
@@ -1,4 +1,11 @@
-const WorkspaceIdPage = () => {
+import { redirect } from "next/navigation";
+
+import { getCurrent } from "@/features/auth/queries";
+
+const WorkspaceIdPage = async () => {
+  const user = await getCurrent();
+  if (!user) redirect("/sign-in");
+
   return (
     <div className="">
       Workspace Id
