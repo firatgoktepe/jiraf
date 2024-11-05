0dcec6116ad3df7e4affbd5bf5ab47f02b895e79
firatgoktepe <firatgoktepe1986@gmail.com>
2024-11-05 15:55:45 +0300

    feat: add workspace join route

--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -1,8 +1,11 @@
+import { z } from "zod";
 import { Hono } from "hono";
 import { zValidator } from "@hono/zod-validator";
 import { ID, Query } from "node-appwrite";
 
 import { createWorkspaceSchema, updateWorkspaceSchema } from "../shemas";
+
+import { Workspace } from "../types";
 
 import { MemberRole } from "@/features/members/types";
 import { getMember } from "@/features/members/utils";
@@ -205,5 +208,50 @@ const app = new Hono()
 
             return c.json({ data: workspace });
         }
-    );
+    )
+    .post(
+        "/:workspaceId/join",
+        sessionMiddleware,
+        zValidator("json", z.object({ code: z.string() })),
+        async (c) => {
+            const databases = c.get("databases");
+            const user = c.get("user");
+
+            const { workspaceId } = c.req.param();
+            const { code } = c.req.valid("json");
+
+            const member = await getMember({
+                databases,
+                workspaceId,
+                userId: user.$id,
+            });
+
+            if (member) {
+                return c.json({ error: "Member already exists" }, 400);
+            }
+
+            const workspace = await databases.getDocument<Workspace>(
+                DATABASE_ID,
+                WORKSPACES_ID,
+                workspaceId,
+            );
+
+            if (workspace.inviteCode !== code) {
+                return c.json({ error: "Invalid invite code" }, 400);
+            }
+            await databases.createDocument(
+                DATABASE_ID,
+                MEMBERS_ID,
+                ID.unique(),
+                {
+                    workspaceId,
+                    userId: user.$id,
+                    role: MemberRole.MEMBER,
+                },
+            );
+
+            return c.json({ data: workspace });
+
+        }
+    )
 export default app;
