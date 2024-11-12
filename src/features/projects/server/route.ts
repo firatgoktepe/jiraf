0a9cec7d74e9b3ce6790c1e227340268f340de60
firatgoktepe <firatgoktepe1986@gmail.com>
2024-11-12 11:11:08 +0300

    feat: create projects api route

---
+++ src/features/projects/server/route.ts
@@ -0,0 +1,49 @@
+import { Hono } from "hono";
+import { Query } from 'node-appwrite';
+
+import { z } from "zod";
+import { zValidator } from "@hono/zod-validator";
+
+import { sessionMiddleware } from "@/lib/session-middleware";
+import { getMember } from "@/features/members/utils";
+import { DATABASE_ID, PROJECTS_ID } from '@/config';
+
+const app = new Hono()
+    .get(
+        "/",
+        sessionMiddleware,
+        zValidator("query", z.object({ workspaceId: z.string() })),
+        async (c) => {
+            const databases = c.get("databases");
+            const user = c.get("user");
+
+            const { workspaceId } = c.req.valid("query");
+
+            const member = await getMember(
+                {
+                    databases,
+                    workspaceId,
+                    userId: user.$id,
+
+                }
+            );
+
+            if (!member) {
+                return c.json({ error: "Unauthorized" }, 401);
+            }
+
+            const projects = await databases.listDocuments(
+                DATABASE_ID,
+                PROJECTS_ID,
+                [
+                    Query.equal("workspaceId", workspaceId),
+                    Query.orderDesc("createdAt"),
+
+                ],
+            )
+
+            return c.json({ data: projects });
+        }
+    )
+
+export default app;
