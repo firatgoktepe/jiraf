003b9d1bfba066d5a9057c2a87f2dcd1bca9dfc8
firatgoktepe <firatgoktepe1986@gmail.com>
2024-11-14 17:48:29 +0300

    feat: create task view switcher

---
+++ src/features/tasks/components/task-view-switcher.tsx
@@ -0,0 +1,46 @@
+import { PlusIcon } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import { Tabs, TabsList, TabsTrigger, TabsContent } from "@/components/ui/tabs";
+
+import { DottedSeparator } from "@/components/dotted-separator";
+
+export const TaskViewSwitcher = () => {
+  return (
+    <Tabs className="flex-1 w-full border rounded-lg">
+      <div className="h-full flex flex-col overflow-auto p-4">
+        <div className="flex flex-col gap-y-2 lg:flex-row justify-between items-center">
+          <TabsList className="w-full lg:w-auto">
+            <TabsTrigger value="table" className="h-8 w-full lg:w-auto">
+              Table
+            </TabsTrigger>
+            <TabsTrigger value="kanban" className="h-8 w-full lg:w-auto">
+              Kanban
+            </TabsTrigger>
+            <TabsTrigger value="calender" className="h-8 w-full lg:w-auto">
+              Calender
+            </TabsTrigger>
+          </TabsList>
+          <Button size="sm" className="w-full lg:w-auto">
+            <PlusIcon className="mr-2 size-4" />
+            New
+          </Button>
+        </div>
+        <DottedSeparator className="my-4" />
+        Data filters
+        <DottedSeparator className="my-4" />
+        <>
+          <TabsContent value="table" className="mt-0">
+            Data table
+          </TabsContent>
+          <TabsContent value="kanban" className="mt-0">
+            Data kanban
+          </TabsContent>
+          <TabsContent value="calender" className="mt-0">
+            Data calender
+          </TabsContent>
+        </>
+      </div>
+    </Tabs>
+  );
+};
