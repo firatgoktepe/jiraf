ae77ee798922b75ffd17558f30ac42394d72530a
firatgoktepe <firatgoktepe1986@gmail.com>
2024-11-19 16:37:21 +0300

    feat: create custom use task filters hook

---
+++ src/features/tasks/hooks/use-task-filters.ts
@@ -0,0 +1,13 @@
+import { useQueryStates, parseAsString, parseAsStringEnum } from "nuqs";
+
+import { TaskStatus } from "../types";
+
+export const useTaskFilters = () => {
+    return useQueryStates({
+        projectId: parseAsString,
+        status: parseAsStringEnum(Object.values(TaskStatus)),
+        assigneeId: parseAsString,
+        search: parseAsString,
+        dueDate: parseAsString,
+    })
+};
