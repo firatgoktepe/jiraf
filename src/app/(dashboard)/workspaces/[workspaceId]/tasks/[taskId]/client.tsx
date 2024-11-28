60dc28d87f578f4e4041cf2a90f62c4aed4c5b49
firatgoktepe <firatgoktepe1986@gmail.com>
2024-11-28 11:54:07 +0300

    feat: add brand new components

--- src/app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/client.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/client.tsx
@@ -1,9 +1,34 @@
 "use client";
 
+import { useGetTask } from "@/features/tasks/api/use-get-task";
+import { useTaskId } from "@/features/tasks/hooks/use-task-id";
+
+import { TaskBreadcrumbs } from "@/features/tasks/components/task-breadcrumbs";
+import { TaskOverview } from "@/features/tasks/components/task-overview";
+
+import { PageLoader } from "@/components/page-loader";
+import { PageError } from "@/components/page-error";
+import { DottedSeparator } from "@/components/dotted-separator";
+
 export const TaskIdClient = () => {
+  const taskId = useTaskId();
+  const { data, isLoading } = useGetTask({ taskId });
+
+  if (isLoading) {
+    return <PageLoader />;
+  }
+
+  if (!data) {
+    return <PageError message="Task not found" />;
+  }
+
   return (
-    <div>
-      <div>Task Id Client</div>
+    <div className="flex flex-col">
+      <TaskBreadcrumbs project={data.project} task={data} />
+      <DottedSeparator className="my-6" />
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
+        <TaskOverview task={data} />
+      </div>
     </div>
   );
 };
