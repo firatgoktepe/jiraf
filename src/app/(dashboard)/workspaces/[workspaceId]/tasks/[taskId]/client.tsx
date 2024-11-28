377a84121d9593c20f3f45927a55cec68611da68
firatgoktepe <firatgoktepe1986@gmail.com>
2024-11-28 17:42:15 +0300

    fix: add additional type & task description comp for tasks modules

--- src/app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/client.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/client.tsx
@@ -5,6 +5,7 @@ import { useTaskId } from "@/features/tasks/hooks/use-task-id";
 
 import { TaskBreadcrumbs } from "@/features/tasks/components/task-breadcrumbs";
 import { TaskOverview } from "@/features/tasks/components/task-overview";
+import { TaskDescription } from "@/features/tasks/components/task-description";
 
 import { PageLoader } from "@/components/page-loader";
 import { PageError } from "@/components/page-error";
@@ -28,6 +29,7 @@ export const TaskIdClient = () => {
       <DottedSeparator className="my-6" />
       <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
         <TaskOverview task={data} />
+        <TaskDescription task={data} />
       </div>
     </div>
   );
