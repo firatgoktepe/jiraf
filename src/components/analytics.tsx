c238c67e7bbb5ca3cf58131b80cd4b92cc4530fb
firatgoktepe <firatgoktepe1986@gmail.com>
2024-11-29 20:05:44 +0300

    feat: add analytics component

---
+++ src/components/analytics.tsx
@@ -0,0 +1,21 @@
+import { ProjectAnalyticsResponseType } from "@/features/projects/api/use-get-project-analytics";
+
+import { ScrollArea } from "./ui/scroll-area";
+import { AnalyticsCard } from "./analytics-card";
+
+export const Analytics = ({ data }: ProjectAnalyticsResponseType) => {
+  return (
+    <ScrollArea className="border rounded-lg w-full whitespace-nowrap shrink-0">
+      <div className="w-full flex flex-row">
+        <div className="flex flex-1 items-center">
+          <AnalyticsCard
+            title="Total tasks"
+            value={data.taskCount}
+            variant={data.taskDifference > 0 ? "up" : "down"}
+            increaseValue={data.taskDifference}
+          />
+        </div>
+      </div>
+    </ScrollArea>
+  );
+};
