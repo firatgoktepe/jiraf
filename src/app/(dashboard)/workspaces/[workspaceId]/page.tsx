0b2a948f8aaf3a028a79b8bd029234963d22e1bb
firatgoktepe <firatgoktepe1986@gmail.com>
2024-11-30 17:50:52 +0300

    feat: add analytics

--- src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
@@ -2,16 +2,13 @@ import { redirect } from "next/navigation";
 
 import { getCurrent } from "@/features/auth/queries";
 
+import { WorkspaceIdClient } from "./client";
+
 const WorkspaceIdPage = async () => {
   const user = await getCurrent();
   if (!user) redirect("/sign-in");
 
-  return (
-    <div className="">
-      Workspace Id
-      <p>This is the workspace id page</p>
-    </div>
-  );
+  return <WorkspaceIdClient />;
 };
 
 export default WorkspaceIdPage;
--- src/components/analytics.tsx
+++ src/components/analytics.tsx
@@ -1,7 +1,8 @@
 import { ProjectAnalyticsResponseType } from "@/features/projects/api/use-get-project-analytics";
 
-import { ScrollArea } from "./ui/scroll-area";
+import { ScrollArea, ScrollBar } from "./ui/scroll-area";
 import { AnalyticsCard } from "./analytics-card";
+import { DottedSeparator } from "./dotted-separator";
 
 export const Analytics = ({ data }: ProjectAnalyticsResponseType) => {
   return (
@@ -9,13 +10,51 @@ export const Analytics = ({ data }: ProjectAnalyticsResponseType) => {
       <div className="w-full flex flex-row">
         <div className="flex flex-1 items-center">
           <AnalyticsCard
-            title="Total tasks"
+            title="Total Tasks"
             value={data.taskCount}
             variant={data.taskDifference > 0 ? "up" : "down"}
             increaseValue={data.taskDifference}
           />
+          <DottedSeparator direction="vertical" />
+        </div>
+        <div className="flex flex-1 items-center">
+          <AnalyticsCard
+            title="Assigned Tasks"
+            value={data.assignedTaskCount}
+            variant={data.assignedTaskDifference > 0 ? "up" : "down"}
+            increaseValue={data.assignedTaskDifference}
+          />
+          <DottedSeparator direction="vertical" />
+        </div>
+        <div className="flex flex-1 items-center">
+          <AnalyticsCard
+            title="Completed Tasks"
+            value={data.completedTaskCount}
+            variant={data.completedTaskDifference > 0 ? "up" : "down"}
+            increaseValue={data.completedTaskDifference}
+          />
+          <DottedSeparator direction="vertical" />
+        </div>
+        <div className="flex flex-1 items-center">
+          <AnalyticsCard
+            title="Overdue Tasks"
+            value={data.overdueTaskCount}
+            variant={data.overdueTaskDifference > 0 ? "up" : "down"}
+            increaseValue={data.overdueTaskDifference}
+          />
+          <DottedSeparator direction="vertical" />
+        </div>
+        <div className="flex flex-1 items-center">
+          <AnalyticsCard
+            title="Incomplete Tasks"
+            value={data.incompleteTaskCount}
+            variant={data.incompleteTaskDifference > 0 ? "up" : "down"}
+            increaseValue={data.incompleteTaskDifference}
+          />
+          <DottedSeparator direction="vertical" />
         </div>
       </div>
+      <ScrollBar orientation="horizontal" />
     </ScrollArea>
   );
 };
